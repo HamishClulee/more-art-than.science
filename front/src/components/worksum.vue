cc4ce26fb852147d2ce3a49bb332c8aa179407e6
HamishClulee <hamish.clulee@hotmail.com>
2019-10-16 09:20:30 +0800

    deploy low quality preview images . Deployed: 2019-10-16 09:20:30

--- front/src/components/worksum.vue
+++ front/src/components/worksum.vue
@@ -4,9 +4,15 @@
         <h4>{{ sub }}</h4>
         <div class="content">
             <div class="img-grid">
-                <img v-for="(v, i) in images" :key="i" :src="images[i]" :class="'img-grid-' + i" />
+                <figure 
+                    v-for="(v, i) in images"
+                    :key="i" :class="'img-grid-' + i"
+                    class="fig-itm"
+                    :style="{ backgroundImage: 'url(' + images[i] + ')'}">
+                </figure>
             </div>
             <div class="text-con" v-html="text"></div>
+            <div class="fin-con">~</div>
         </div>
     </div>
 </template>
@@ -24,4 +30,38 @@
 </script>
 <style lang="sass" scoped>
 .sum-con
+    width: 90%
+    margin-left: auto
+    margin-right: auto
+    margin-bottom: 100px
+.fin-con
+    font-size: 3em
+    width: 100%
+    height: 80px
+    display: flex
+    align-items: center
+    justify-content: center
+.fig-itm
+    border: 1px solid #adadad
+    background-repeat: no-repeat
+    background-size: cover
+figure
+    margin: 0
+.img-grid
+    display: grid
+    grid-template-columns: 33% 33% 33%
+    grid-gap: 2px
+    height: 50vh
+    width: 100%
+.img-grid-0
+    grid-column: 1 / span 2
+.img-grid-1
+    grid-column: 3
+    grid-row: 1 / span 2
+.img-grid-2
+    grid-column: 1
+    grid-row: 2
+.img-grid-3
+    grid-column: 2
+    grid-row: 2
 </style>
--- front/src/views/work.vue
+++ front/src/views/work.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="work-con">
-        <worksum title="@RONIN" sub="Website" :images="[]" :text="ronin_txt"></worksum>
-        <worksum title="Netograph" sub="Web App" :images="[]" :text="neto_txt"></worksum>
-        <worksum title="Baby Aid NZ" sub="Web App" :images="[]" :text="babyaid_txt"></worksum>
-        <worksum title="Dashboard Builder" sub="Web App" :images="[]" :text="dbb_txt"></worksum>
+        <worksum title="@RONIN" sub="Website" :images="roninimg" :text="ronin_txt"></worksum>
+        <worksum title="Netograph" sub="Web App" :images="netimg" :text="neto_txt"></worksum>
+        <worksum title="Baby Aid NZ" sub="Web App" :images="babimg" :text="babyaid_txt"></worksum>
+        <worksum title="Dashboard Builder" sub="Web App" :images="dbbimg" :text="dbb_txt"></worksum>
     </div>
 </template>
 
@@ -14,11 +14,34 @@ export default {
     components: { worksum },
     data () {
         return {
-            ronin_txt: `s`,
-            neto_txt: `s`,
-            babyaid_txt: `s`,
-            dbb_txt: `s`,
-            images: [],
+            ronin_txt: `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas efficitur pellentesque ligula et pharetra. Sed quis ultricies massa, a molestie eros. Integer pellentesque nibh nec purus efficitur, id tempor eros egestas. Aenean lacinia in magna nec ultrices. Nullam at laoreet magna, et gravida diam</p>`,
+            neto_txt: `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas efficitur pellentesque ligula et pharetra. Sed quis ultricies massa, a molestie eros. Integer pellentesque nibh nec purus efficitur, id tempor eros egestas. Aenean lacinia in magna nec ultrices. Nullam at laoreet magna, et gravida diam</p>`,
+            babyaid_txt: `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas efficitur pellentesque ligula et pharetra. Sed quis ultricies massa, a molestie eros. Integer pellentesque nibh nec purus efficitur, id tempor eros egestas. Aenean lacinia in magna nec ultrices. Nullam at laoreet magna, et gravida diam</p>`,
+            dbb_txt: `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas efficitur pellentesque ligula et pharetra. Sed quis ultricies massa, a molestie eros. Integer pellentesque nibh nec purus efficitur, id tempor eros egestas. Aenean lacinia in magna nec ultrices. Nullam at laoreet magna, et gravida diam</p>`,
+            babimg: [
+                "https://more-art-than.science/images/bab1-lowqual.jpg",
+                "https://more-art-than.science/images/bab3-lowqual.jpg",
+                "https://more-art-than.science/images/bab2-lowqual.jpg",
+                "https://more-art-than.science/images/bab4-lowqual.jpg",
+            ],
+            netimg: [
+                "https://more-art-than.science/images/neto1-lowqual.jpg",
+                "https://more-art-than.science/images/neto2-lowqual.jpg",
+                "https://more-art-than.science/images/neto3-lowqual.jpg",
+                "https://more-art-than.science/images/neto4-lowqual.jpg",
+            ],
+            dbbimg: [
+                "https://more-art-than.science/images/dbb1-lowqual.jpg",
+                "https://more-art-than.science/images/dbb2-lowqual.jpg",
+                "https://more-art-than.science/images/dbb3-lowqual.jpg",
+                "https://more-art-than.science/images/dbb4-lowqual.jpg",
+            ],
+            roninimg: [
+                "https://more-art-than.science/images/ronin1-lowqual.jpg",
+                "https://more-art-than.science/images/ronin2-lowqual.jpg",
+                "https://more-art-than.science/images/ronin3-lowqual.jpg",
+                "https://more-art-than.science/images/ronin4-lowqual.jpg",
+            ]
         }
     },
     mounted () {
@@ -35,4 +58,5 @@ export default {
     width: 95%
     margin-left: auto
     margin-right: auto
+    margin-bottom: 200px
 </style>

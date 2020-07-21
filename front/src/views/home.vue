ba6ed4ed231aa11ac712d019789a9f0ea6a307c4
HamishClulee <hamish.clulee@hotmail.com>
2020-07-21 17:15:59 +0700

    Update linkedin href . Deployed: 2020-07-21 17:15:59

--- front/src/views/home.vue
+++ front/src/views/home.vue
@@ -25,7 +25,7 @@
             <a href="https://github.com/HamishClulee" target="_blank" class="icon-container-item">
                 <img src="https://more-art-than.science/images/github.svg" height="30"/>
             </a>
-            <a href="https://www.linkedin.com/in/hamish-clulee-163b1950/" target="_blank" class="icon-container-item">
+            <a href="https://www.linkedin.com/in/hamish-clulee/" target="_blank" class="icon-container-item">
                 <img src="https://more-art-than.science/images/linkedin.svg" height="30" />
             </a>
         </div>
